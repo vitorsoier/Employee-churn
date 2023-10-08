b54e75aec91a8303db6a039dd16ee271b7106a98
Vitor Soier <vitorsoier1@gmail.com>
2023-10-08 15:57:45 -0300

    Changing the extraction function and add some information in the main

--- dados/extract.py
+++ dados/extract.py
@@ -1,21 +1,26 @@
 from datetime import datetime
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def log(message):
-    timestamp_format = '%Y-%h-%d-%H:%M:%S' 
-    now = datetime.now() 
-    timestamp = now.strftime(timestamp_format)
-    with open("dados\logfile.txt","a") as f:
-        f.write(message +  ', ' + timestamp + '\n')
 
-def extract():
-    log('Iniciando chamada API')
+def log(message):
+    timestamp_format = "%Y-%h-%d-%H:%M:%S"
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open("dados\logfile.txt", "a") as f:
+        f.write(message + ", " + timestamp + "\n")
+
+
+def extract(page, dataset):
+    log(f"Iniciando chamada API para o dataset {dataset}")
     api = KaggleApi()
     api.authenticate()
-    log('Api autenticada')
-    api.dataset_download_file('tawfikelmetwally/employee-dataset', file_name = 'Employee.csv', path= 'dados')
-    log('Dados Salvos')
+    log(f"Api autenticada para o dataset {dataset}")
+    api.dataset_download_file(page, file_name=dataset, path="dados")
+    log(f"Dados Salvos para o dataset {dataset}")
+    log(f"Processo finalizado para o dataset {dataset}")
 
-extract()
-log('Processo finalizado')
 
+extract(
+    "pavansubhasht/ibm-hr-analytics-attrition-dataset",
+    "WA_Fn-UseC_-HR-Employee-Attrition.csv",
+)
